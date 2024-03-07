eb7a9899bac88928f00541d6372ee1f5b21d7a60
Yidan Xu <112134918+yidanxu92@users.noreply.github.com>
2024-03-07 16:26:28 -0500

    update background picture

--- src/components/top/Top.js
+++ src/components/top/Top.js
@@ -9,7 +9,8 @@ const Top = () => {
     return (
         <div id="top"
         className="title bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0" 
-        style={{ background:`url("rivendall.avif")`}}>
+        style={{ background:`url("rivendall.avif")`,backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",}}>
             <div id="stars"></div>
             <Container className="text-center">
                 <h1 className="display-1">Yidan Xu</h1>
